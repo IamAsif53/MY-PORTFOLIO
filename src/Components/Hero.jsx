72cd0c312095ed543bc1fca3b578fc379b8f5341
Jiaul Asif <u2104053@student.cuet.ac.bd>
2026-01-06 00:30:50 +0600

    Done for now

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -59,7 +59,7 @@ export default function Hero() {
           <p className="mt-6 text-base md:text-lg text-gray-400 leading-relaxed max-w-xl">
             I specialize in React, Node,Tailwind CSS, and modern JavaScript —
             focusing on clean architecture, performance, and delightful user
-            experiences. aksldjflkasdjf;akldsjfkaldsjfk
+            experiences.
           </p>
         </div>
 
